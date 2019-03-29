557aac0442f36af110d058c583f44de6bed123c9
MrConan1 <36061999+MrConan1@users.noreply.github.com>
2019-03-29 15:40:45 -0400

    updated for new encode input

--- test/test_sss.bat
+++ test/test_sss.bat
@@ -1,309 +1,309 @@
 lsb decode sss_script\TEXT000.DAT TEXT000.txt 0 sss
-lsb encode TEXT000.txt TEXT000_reenc.DAT sss
+lsb encode TEXT000.txt TEXT000_reenc.DAT 0 sss
 fc /b sss_script\TEXT000.DAT TEXT000_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT001.DAT TEXT001.txt 0 sss
-lsb encode TEXT001.txt TEXT001_reenc.DAT sss
+lsb encode TEXT001.txt TEXT001_reenc.DAT 0 sss
 fc /b sss_script\TEXT001.DAT TEXT001_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT002.DAT TEXT002.txt 0 sss
-lsb encode TEXT002.txt TEXT002_reenc.DAT sss
+lsb encode TEXT002.txt TEXT002_reenc.DAT 0 sss
 fc /b sss_script\TEXT002.DAT TEXT002_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT003.DAT TEXT003.txt 0 sss
-lsb encode TEXT003.txt TEXT003_reenc.DAT sss
+lsb encode TEXT003.txt TEXT003_reenc.DAT 0 sss
 fc /b sss_script\TEXT003.DAT TEXT003_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT004.DAT TEXT004.txt 0 sss
-lsb encode TEXT004.txt TEXT004_reenc.DAT sss
+lsb encode TEXT004.txt TEXT004_reenc.DAT 0 sss
 fc /b sss_script\TEXT004.DAT TEXT004_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT005.DAT TEXT005.txt 0 sss
-lsb encode TEXT005.txt TEXT005_reenc.DAT sss
+lsb encode TEXT005.txt TEXT005_reenc.DAT 0 sss
 fc /b sss_script\TEXT005.DAT TEXT005_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT006.DAT TEXT006.txt 0 sss
-lsb encode TEXT006.txt TEXT006_reenc.DAT sss
+lsb encode TEXT006.txt TEXT006_reenc.DAT 0 sss
 fc /b sss_script\TEXT006.DAT TEXT006_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT007.DAT TEXT007.txt 0 sss
-lsb encode TEXT007.txt TEXT007_reenc.DAT sss
+lsb encode TEXT007.txt TEXT007_reenc.DAT 0 sss
 fc /b sss_script\TEXT007.DAT TEXT007_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT008.DAT TEXT008.txt 0 sss
-lsb encode TEXT008.txt TEXT008_reenc.DAT sss
+lsb encode TEXT008.txt TEXT008_reenc.DAT 0 sss
 fc /b sss_script\TEXT008.DAT TEXT008_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT009.DAT TEXT009.txt 0 sss
-lsb encode TEXT009.txt TEXT009_reenc.DAT sss
+lsb encode TEXT009.txt TEXT009_reenc.DAT 0 sss
 fc /b sss_script\TEXT009.DAT TEXT009_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT010.DAT TEXT010.txt 0 sss
-lsb encode TEXT010.txt TEXT010_reenc.DAT sss
+lsb encode TEXT010.txt TEXT010_reenc.DAT 0 sss
 fc /b sss_script\TEXT010.DAT TEXT010_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT011.DAT TEXT011.txt 0 sss
-lsb encode TEXT011.txt TEXT011_reenc.DAT sss
+lsb encode TEXT011.txt TEXT011_reenc.DAT 0 sss
 fc /b sss_script\TEXT011.DAT TEXT011_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT012.DAT TEXT012.txt 0 sss
-lsb encode TEXT012.txt TEXT012_reenc.DAT sss
+lsb encode TEXT012.txt TEXT012_reenc.DAT 0 sss
 fc /b sss_script\TEXT012.DAT TEXT012_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT013.DAT TEXT013.txt 0 sss
-lsb encode TEXT013.txt TEXT013_reenc.DAT sss
+lsb encode TEXT013.txt TEXT013_reenc.DAT 0 sss
 fc /b sss_script\TEXT013.DAT TEXT013_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT014.DAT TEXT014.txt 0 sss
-lsb encode TEXT014.txt TEXT014_reenc.DAT sss
+lsb encode TEXT014.txt TEXT014_reenc.DAT 0 sss
 fc /b sss_script\TEXT014.DAT TEXT014_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT015.DAT TEXT015.txt 0 sss
-lsb encode TEXT015.txt TEXT015_reenc.DAT sss
+lsb encode TEXT015.txt TEXT015_reenc.DAT 0 sss
 fc /b sss_script\TEXT015.DAT TEXT015_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT016.DAT TEXT016.txt 0 sss
-lsb encode TEXT016.txt TEXT016_reenc.DAT sss
+lsb encode TEXT016.txt TEXT016_reenc.DAT 0 sss
 fc /b sss_script\TEXT016.DAT TEXT016_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT017.DAT TEXT017.txt 0 sss
-lsb encode TEXT017.txt TEXT017_reenc.DAT sss
+lsb encode TEXT017.txt TEXT017_reenc.DAT 0 sss
 fc /b sss_script\TEXT017.DAT TEXT017_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT018.DAT TEXT018.txt 0 sss
-lsb encode TEXT018.txt TEXT018_reenc.DAT sss
+lsb encode TEXT018.txt TEXT018_reenc.DAT 0 sss
 fc /b sss_script\TEXT018.DAT TEXT018_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT019.DAT TEXT019.txt 0 sss
-lsb encode TEXT019.txt TEXT019_reenc.DAT sss
+lsb encode TEXT019.txt TEXT019_reenc.DAT 0 sss
 fc /b sss_script\TEXT019.DAT TEXT019_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT020.DAT TEXT020.txt 0 sss
-lsb encode TEXT020.txt TEXT020_reenc.DAT sss
+lsb encode TEXT020.txt TEXT020_reenc.DAT 0 sss
 fc /b sss_script\TEXT020.DAT TEXT020_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT021.DAT TEXT021.txt 0 sss
-lsb encode TEXT021.txt TEXT021_reenc.DAT sss
+lsb encode TEXT021.txt TEXT021_reenc.DAT 0 sss
 fc /b sss_script\TEXT021.DAT TEXT021_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT022.DAT TEXT022.txt 0 sss
-lsb encode TEXT022.txt TEXT022_reenc.DAT sss
+lsb encode TEXT022.txt TEXT022_reenc.DAT 0 sss
 fc /b sss_script\TEXT022.DAT TEXT022_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT023.DAT TEXT023.txt 0 sss
-lsb encode TEXT023.txt TEXT023_reenc.DAT sss
+lsb encode TEXT023.txt TEXT023_reenc.DAT 0 sss
 fc /b sss_script\TEXT023.DAT TEXT023_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT024.DAT TEXT024.txt 0 sss
-lsb encode TEXT024.txt TEXT024_reenc.DAT sss
+lsb encode TEXT024.txt TEXT024_reenc.DAT 0 sss
 fc /b sss_script\TEXT024.DAT TEXT024_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT025.DAT TEXT025.txt 0 sss
-lsb encode TEXT025.txt TEXT025_reenc.DAT sss
+lsb encode TEXT025.txt TEXT025_reenc.DAT 0 sss
 fc /b sss_script\TEXT025.DAT TEXT025_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT026.DAT TEXT026.txt 0 sss
-lsb encode TEXT026.txt TEXT026_reenc.DAT sss
+lsb encode TEXT026.txt TEXT026_reenc.DAT 0 sss
 fc /b sss_script\TEXT026.DAT TEXT026_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT027.DAT TEXT027.txt 0 sss
-lsb encode TEXT027.txt TEXT027_reenc.DAT sss
+lsb encode TEXT027.txt TEXT027_reenc.DAT 0 sss
 fc /b sss_script\TEXT027.DAT TEXT027_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT028.DAT TEXT028.txt 0 sss
-lsb encode TEXT028.txt TEXT028_reenc.DAT sss
+lsb encode TEXT028.txt TEXT028_reenc.DAT 0 sss
 fc /b sss_script\TEXT028.DAT TEXT028_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT029.DAT TEXT029.txt 0 sss
-lsb encode TEXT029.txt TEXT029_reenc.DAT sss
+lsb encode TEXT029.txt TEXT029_reenc.DAT 0 sss
 fc /b sss_script\TEXT029.DAT TEXT029_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT030.DAT TEXT030.txt 0 sss
-lsb encode TEXT030.txt TEXT030_reenc.DAT sss
+lsb encode TEXT030.txt TEXT030_reenc.DAT 0 sss
 fc /b sss_script\TEXT030.DAT TEXT030_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT031.DAT TEXT031.txt 0 sss
-lsb encode TEXT031.txt TEXT031_reenc.DAT sss
+lsb encode TEXT031.txt TEXT031_reenc.DAT 0 sss
 fc /b sss_script\TEXT031.DAT TEXT031_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT032.DAT TEXT032.txt 0 sss
-lsb encode TEXT032.txt TEXT032_reenc.DAT sss
+lsb encode TEXT032.txt TEXT032_reenc.DAT 0 sss
 fc /b sss_script\TEXT032.DAT TEXT032_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT033.DAT TEXT033.txt 0 sss
-lsb encode TEXT033.txt TEXT033_reenc.DAT sss
+lsb encode TEXT033.txt TEXT033_reenc.DAT 0 sss
 fc /b sss_script\TEXT033.DAT TEXT033_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT034.DAT TEXT034.txt 0 sss
-lsb encode TEXT034.txt TEXT034_reenc.DAT sss
+lsb encode TEXT034.txt TEXT034_reenc.DAT 0 sss
 fc /b sss_script\TEXT034.DAT TEXT034_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT035.DAT TEXT035.txt 0 sss
-lsb encode TEXT035.txt TEXT035_reenc.DAT sss
+lsb encode TEXT035.txt TEXT035_reenc.DAT 0 sss
 fc /b sss_script\TEXT035.DAT TEXT035_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT036.DAT TEXT036.txt 0 sss
-lsb encode TEXT036.txt TEXT036_reenc.DAT sss
+lsb encode TEXT036.txt TEXT036_reenc.DAT 0 sss
 fc /b sss_script\TEXT036.DAT TEXT036_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT037.DAT TEXT037.txt 0 sss
-lsb encode TEXT037.txt TEXT037_reenc.DAT sss
+lsb encode TEXT037.txt TEXT037_reenc.DAT 0 sss
 fc /b sss_script\TEXT037.DAT TEXT037_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT038.DAT TEXT038.txt 0 sss
-lsb encode TEXT038.txt TEXT038_reenc.DAT sss
+lsb encode TEXT038.txt TEXT038_reenc.DAT 0 sss
 fc /b sss_script\TEXT038.DAT TEXT038_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT039.DAT TEXT039.txt 0 sss
-lsb encode TEXT039.txt TEXT039_reenc.DAT sss
+lsb encode TEXT039.txt TEXT039_reenc.DAT 0 sss
 fc /b sss_script\TEXT039.DAT TEXT039_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT040.DAT TEXT040.txt 0 sss
-lsb encode TEXT040.txt TEXT040_reenc.DAT sss
+lsb encode TEXT040.txt TEXT040_reenc.DAT 0 sss
 fc /b sss_script\TEXT040.DAT TEXT040_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT041.DAT TEXT041.txt 0 sss
-lsb encode TEXT041.txt TEXT041_reenc.DAT sss
+lsb encode TEXT041.txt TEXT041_reenc.DAT 0 sss
 fc /b sss_script\TEXT041.DAT TEXT041_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT042.DAT TEXT042.txt 0 sss
-lsb encode TEXT042.txt TEXT042_reenc.DAT sss
+lsb encode TEXT042.txt TEXT042_reenc.DAT 0 sss
 fc /b sss_script\TEXT042.DAT TEXT042_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT043.DAT TEXT043.txt 0 sss
-lsb encode TEXT043.txt TEXT043_reenc.DAT sss
+lsb encode TEXT043.txt TEXT043_reenc.DAT 0 sss
 fc /b sss_script\TEXT043.DAT TEXT043_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT044.DAT TEXT044.txt 0 sss
-lsb encode TEXT044.txt TEXT044_reenc.DAT sss
+lsb encode TEXT044.txt TEXT044_reenc.DAT 0 sss
 fc /b sss_script\TEXT044.DAT TEXT044_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT045.DAT TEXT045.txt 0 sss
-lsb encode TEXT045.txt TEXT045_reenc.DAT sss
+lsb encode TEXT045.txt TEXT045_reenc.DAT 0 sss
 fc /b sss_script\TEXT045.DAT TEXT045_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT046.DAT TEXT046.txt 0 sss
-lsb encode TEXT046.txt TEXT046_reenc.DAT sss
+lsb encode TEXT046.txt TEXT046_reenc.DAT 0 sss
 fc /b sss_script\TEXT046.DAT TEXT046_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT047.DAT TEXT047.txt 0 sss
-lsb encode TEXT047.txt TEXT047_reenc.DAT sss
+lsb encode TEXT047.txt TEXT047_reenc.DAT 0 sss
 fc /b sss_script\TEXT047.DAT TEXT047_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT048.DAT TEXT048.txt 0 sss
-lsb encode TEXT048.txt TEXT048_reenc.DAT sss
+lsb encode TEXT048.txt TEXT048_reenc.DAT 0 sss
 fc /b sss_script\TEXT048.DAT TEXT048_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT049.DAT TEXT049.txt 0 sss
-lsb encode TEXT049.txt TEXT049_reenc.DAT sss
+lsb encode TEXT049.txt TEXT049_reenc.DAT 0 sss
 fc /b sss_script\TEXT049.DAT TEXT049_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT050.DAT TEXT050.txt 0 sss
-lsb encode TEXT050.txt TEXT050_reenc.DAT sss
+lsb encode TEXT050.txt TEXT050_reenc.DAT 0 sss
 fc /b sss_script\TEXT050.DAT TEXT050_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT051.DAT TEXT051.txt 0 sss
-lsb encode TEXT051.txt TEXT051_reenc.DAT sss
+lsb encode TEXT051.txt TEXT051_reenc.DAT 0 sss
 fc /b sss_script\TEXT051.DAT TEXT051_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT052.DAT TEXT052.txt 0 sss
-lsb encode TEXT052.txt TEXT052_reenc.DAT sss
+lsb encode TEXT052.txt TEXT052_reenc.DAT 0 sss
 fc /b sss_script\TEXT052.DAT TEXT052_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT053.DAT TEXT053.txt 0 sss
-lsb encode TEXT053.txt TEXT053_reenc.DAT sss
+lsb encode TEXT053.txt TEXT053_reenc.DAT 0 sss
 fc /b sss_script\TEXT053.DAT TEXT053_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT054.DAT TEXT054.txt 0 sss
-lsb encode TEXT054.txt TEXT054_reenc.DAT sss
+lsb encode TEXT054.txt TEXT054_reenc.DAT 0 sss
 fc /b sss_script\TEXT054.DAT TEXT054_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT055.DAT TEXT055.txt 0 sss
-lsb encode TEXT055.txt TEXT055_reenc.DAT sss
+lsb encode TEXT055.txt TEXT055_reenc.DAT 0 sss
 fc /b sss_script\TEXT055.DAT TEXT055_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT056.DAT TEXT056.txt 0 sss
-lsb encode TEXT056.txt TEXT056_reenc.DAT sss
+lsb encode TEXT056.txt TEXT056_reenc.DAT 0 sss
 fc /b sss_script\TEXT056.DAT TEXT056_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT057.DAT TEXT057.txt 0 sss
-lsb encode TEXT057.txt TEXT057_reenc.DAT sss
+lsb encode TEXT057.txt TEXT057_reenc.DAT 0 sss
 fc /b sss_script\TEXT057.DAT TEXT057_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT058.DAT TEXT058.txt 0 sss
-lsb encode TEXT058.txt TEXT058_reenc.DAT sss
+lsb encode TEXT058.txt TEXT058_reenc.DAT 0 sss
 fc /b sss_script\TEXT058.DAT TEXT058_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT059.DAT TEXT059.txt 0 sss
-lsb encode TEXT059.txt TEXT059_reenc.DAT sss
+lsb encode TEXT059.txt TEXT059_reenc.DAT 0 sss
 fc /b sss_script\TEXT059.DAT TEXT059_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT100.DAT TEXT100.txt 0 sss
-lsb encode TEXT100.txt TEXT100_reenc.DAT sss
+lsb encode TEXT100.txt TEXT100_reenc.DAT 0 sss
 fc /b sss_script\TEXT100.DAT TEXT100_reenc.DAT
 pause
 
 lsb decode sss_script\TEXT200.DAT TEXT200.txt 0 sss
-lsb encode TEXT200.txt TEXT200_reenc.DAT sss
+lsb encode TEXT200.txt TEXT200_reenc.DAT 0 sss
 fc /b sss_script\TEXT200.DAT TEXT200_reenc.DAT
 pause
