cad02eaa61f6d57cebf1675036349692d8e600c8
MrConan1 <36061999+MrConan1@users.noreply.github.com>
2019-08-14 22:05:38 -0400

    compression batch test

---
+++ test/test_ios_compr.bat
@@ -0,0 +1,371 @@
+REM Uses JP Characters
+REM lsb decode ios_utf8_script_us\TEXT000.DAT TEXT000.txt 3
+REM lsb encode TEXT000.txt bpetxt000.bin 1 
+REM lsb decode bpetxt000.bin text000.script.txt 1
+REM fc /b TEXT000.txt text000.script.txt
+REM pause
+
+lsb decode ios_utf8_script_us\TEXT001.DAT TEXT001.txt 3
+lsb encode TEXT001.txt bpetxt001.bin 1 
+lsb decode bpetxt001.bin text001.script.txt 1
+fc /b TEXT001.txt text001.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT002.DAT TEXT002.txt 3
+lsb encode TEXT002.txt bpetxt002.bin 1 
+lsb decode bpetxt002.bin text002.script.txt 1
+fc /b TEXT002.txt text002.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT003.DAT TEXT003.txt 3
+lsb encode TEXT003.txt bpetxt003.bin 1 
+lsb decode bpetxt003.bin text003.script.txt 1
+fc /b TEXT003.txt text003.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT004.DAT TEXT004.txt 3
+lsb encode TEXT004.txt bpetxt004.bin 1 
+lsb decode bpetxt004.bin text004.script.txt 1
+fc /b TEXT004.txt text004.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT005.DAT TEXT005.txt 3
+lsb encode TEXT005.txt bpetxt005.bin 1 
+lsb decode bpetxt005.bin text005.script.txt 1
+fc /b TEXT005.txt text005.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT006.DAT TEXT006.txt 3
+lsb encode TEXT006.txt bpetxt006.bin 1 
+lsb decode bpetxt006.bin text006.script.txt 1
+fc /b TEXT006.txt text006.script.txt
+pause
+
+REM Uses JP Characters
+REM lsb decode ios_utf8_script_us\TEXT007.DAT TEXT007.txt 3
+REM lsb encode TEXT007.txt bpetxt007.bin 1 
+REM lsb decode bpetxt007.bin text007.script.txt 1
+REM fc /b TEXT007.txt text007.script.txt
+REM pause
+
+lsb decode ios_utf8_script_us\TEXT008.DAT TEXT008.txt 3
+lsb encode TEXT008.txt bpetxt008.bin 1 
+lsb decode bpetxt008.bin text008.script.txt 1
+fc /b TEXT008.txt text008.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT009.DAT TEXT009.txt 3
+lsb encode TEXT009.txt bpetxt009.bin 1 
+lsb decode bpetxt009.bin text009.script.txt 1
+fc /b TEXT009.txt text009.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT010.DAT TEXT010.txt 3
+lsb encode TEXT010.txt bpetxt010.bin 1 
+lsb decode bpetxt010.bin text010.script.txt 1
+fc /b TEXT010.txt text010.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT011.DAT TEXT011.txt 3
+lsb encode TEXT011.txt bpetxt011.bin 1 
+lsb decode bpetxt011.bin text011.script.txt 1
+fc /b TEXT011.txt text011.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT012.DAT TEXT012.txt 3
+lsb encode TEXT012.txt bpetxt012.bin 1 
+lsb decode bpetxt012.bin text012.script.txt 1
+fc /b TEXT012.txt text012.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT013.DAT TEXT013.txt 3
+lsb encode TEXT013.txt bpetxt013.bin 1 
+lsb decode bpetxt013.bin text013.script.txt 1
+fc /b TEXT013.txt text013.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT014.DAT TEXT014.txt 3
+lsb encode TEXT014.txt bpetxt014.bin 1 
+lsb decode bpetxt014.bin text014.script.txt 1
+fc /b TEXT014.txt text014.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT015.DAT TEXT015.txt 3
+lsb encode TEXT015.txt bpetxt015.bin 1 
+lsb decode bpetxt015.bin text015.script.txt 1
+fc /b TEXT015.txt text015.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT016.DAT TEXT016.txt 3
+lsb encode TEXT016.txt bpetxt016.bin 1 
+lsb decode bpetxt016.bin text016.script.txt 1
+fc /b TEXT016.txt text016.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT017.DAT TEXT017.txt 3
+lsb encode TEXT017.txt bpetxt017.bin 1 
+lsb decode bpetxt017.bin text017.script.txt 1
+fc /b TEXT017.txt text017.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT018.DAT TEXT018.txt 3
+lsb encode TEXT018.txt bpetxt018.bin 1 
+lsb decode bpetxt018.bin text018.script.txt 1
+fc /b TEXT018.txt text018.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT019.DAT TEXT019.txt 3
+lsb encode TEXT019.txt bpetxt019.bin 1 
+lsb decode bpetxt019.bin text019.script.txt 1
+fc /b TEXT019.txt text019.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT020.DAT TEXT020.txt 3
+lsb encode TEXT020.txt bpetxt020.bin 1 
+lsb decode bpetxt020.bin text020.script.txt 1
+fc /b TEXT020.txt text020.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT021.DAT TEXT021.txt 3
+lsb encode TEXT021.txt bpetxt021.bin 1 
+lsb decode bpetxt021.bin text021.script.txt 1
+fc /b TEXT021.txt text021.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT022.DAT TEXT022.txt 3
+lsb encode TEXT022.txt bpetxt022.bin 1 
+lsb decode bpetxt022.bin text022.script.txt 1
+fc /b TEXT022.txt text022.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT023.DAT TEXT023.txt 3
+lsb encode TEXT023.txt bpetxt023.bin 1 
+lsb decode bpetxt023.bin text023.script.txt 1
+fc /b TEXT023.txt text023.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT024.DAT TEXT024.txt 3
+lsb encode TEXT024.txt bpetxt024.bin 1 
+lsb decode bpetxt024.bin text024.script.txt 1
+fc /b TEXT024.txt text024.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT025.DAT TEXT025.txt 3
+lsb encode TEXT025.txt bpetxt025.bin 1 
+lsb decode bpetxt025.bin text025.script.txt 1
+fc /b TEXT025.txt text025.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT026.DAT TEXT026.txt 3
+lsb encode TEXT026.txt bpetxt026.bin 1 
+lsb decode bpetxt026.bin text026.script.txt 1
+fc /b TEXT026.txt text026.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT027.DAT TEXT027.txt 3
+lsb encode TEXT027.txt bpetxt027.bin 1 
+lsb decode bpetxt027.bin text027.script.txt 1
+fc /b TEXT027.txt text027.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT028.DAT TEXT028.txt 3
+lsb encode TEXT028.txt bpetxt028.bin 1 
+lsb decode bpetxt028.bin text028.script.txt 1
+fc /b TEXT028.txt text028.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT029.DAT TEXT029.txt 3
+lsb encode TEXT029.txt bpetxt029.bin 1 
+lsb decode bpetxt029.bin text029.script.txt 1
+fc /b TEXT029.txt text029.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT030.DAT TEXT030.txt 3
+lsb encode TEXT030.txt bpetxt030.bin 1 
+lsb decode bpetxt030.bin text030.script.txt 1
+fc /b TEXT030.txt text030.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT031.DAT TEXT031.txt 3
+lsb encode TEXT031.txt bpetxt031.bin 1 
+lsb decode bpetxt031.bin text031.script.txt 1
+fc /b TEXT031.txt text031.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT032.DAT TEXT032.txt 3
+lsb encode TEXT032.txt bpetxt032.bin 1 
+lsb decode bpetxt032.bin text032.script.txt 1
+fc /b TEXT032.txt text032.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT033.DAT TEXT033.txt 3
+lsb encode TEXT033.txt bpetxt033.bin 1 
+lsb decode bpetxt033.bin text033.script.txt 1
+fc /b TEXT033.txt text033.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT034.DAT TEXT034.txt 3
+lsb encode TEXT034.txt bpetxt034.bin 1 
+lsb decode bpetxt034.bin text034.script.txt 1
+fc /b TEXT034.txt text034.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT035.DAT TEXT035.txt 3
+lsb encode TEXT035.txt bpetxt035.bin 1 
+lsb decode bpetxt035.bin text035.script.txt 1
+fc /b TEXT035.txt text035.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT036.DAT TEXT036.txt 3
+lsb encode TEXT036.txt bpetxt036.bin 1 
+lsb decode bpetxt036.bin text036.script.txt 1
+fc /b TEXT036.txt text036.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT037.DAT TEXT037.txt 3
+lsb encode TEXT037.txt bpetxt037.bin 1 
+lsb decode bpetxt037.bin text037.script.txt 1
+fc /b TEXT037.txt text037.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT038.DAT TEXT038.txt 3
+lsb encode TEXT038.txt bpetxt038.bin 1 
+lsb decode bpetxt038.bin text038.script.txt 1
+fc /b TEXT038.txt text038.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT039.DAT TEXT039.txt 3
+lsb encode TEXT039.txt bpetxt039.bin 1 
+lsb decode bpetxt039.bin text039.script.txt 1
+fc /b TEXT039.txt text039.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT040.DAT TEXT040.txt 3
+lsb encode TEXT040.txt bpetxt040.bin 1 
+lsb decode bpetxt040.bin text040.script.txt 1
+fc /b TEXT040.txt text040.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT041.DAT TEXT041.txt 3
+lsb encode TEXT041.txt bpetxt041.bin 1 
+lsb decode bpetxt041.bin text041.script.txt 1
+fc /b TEXT041.txt text041.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT042.DAT TEXT042.txt 3
+lsb encode TEXT042.txt bpetxt042.bin 1 
+lsb decode bpetxt042.bin text042.script.txt 1
+fc /b TEXT042.txt text042.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT043.DAT TEXT043.txt 3
+lsb encode TEXT043.txt bpetxt043.bin 1 
+lsb decode bpetxt043.bin text043.script.txt 1
+fc /b TEXT043.txt text043.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT044.DAT TEXT044.txt 3
+lsb encode TEXT044.txt bpetxt044.bin 1 
+lsb decode bpetxt044.bin text044.script.txt 1
+fc /b TEXT044.txt text044.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT045.DAT TEXT045.txt 3
+lsb encode TEXT045.txt bpetxt045.bin 1 
+lsb decode bpetxt045.bin text045.script.txt 1
+fc /b TEXT045.txt text045.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT046.DAT TEXT046.txt 3
+lsb encode TEXT046.txt bpetxt046.bin 1 
+lsb decode bpetxt046.bin text046.script.txt 1
+fc /b TEXT046.txt text046.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT047.DAT TEXT047.txt 3
+lsb encode TEXT047.txt bpetxt047.bin 1 
+lsb decode bpetxt047.bin text047.script.txt 1
+fc /b TEXT047.txt text047.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT048.DAT TEXT048.txt 3
+lsb encode TEXT048.txt bpetxt048.bin 1 
+lsb decode bpetxt048.bin text048.script.txt 1
+fc /b TEXT048.txt text048.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT049.DAT TEXT049.txt 3
+lsb encode TEXT049.txt bpetxt049.bin 1 
+lsb decode bpetxt049.bin text049.script.txt 1
+fc /b TEXT049.txt text049.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT050.DAT TEXT050.txt 3
+lsb encode TEXT050.txt bpetxt050.bin 1 
+lsb decode bpetxt050.bin text050.script.txt 1
+fc /b TEXT050.txt text050.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT051.DAT TEXT051.txt 3
+lsb encode TEXT051.txt bpetxt051.bin 1 
+lsb decode bpetxt051.bin text051.script.txt 1
+fc /b TEXT051.txt text051.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT052.DAT TEXT052.txt 3
+lsb encode TEXT052.txt bpetxt052.bin 1 
+lsb decode bpetxt052.bin text052.script.txt 1
+fc /b TEXT052.txt text052.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT053.DAT TEXT053.txt 3
+lsb encode TEXT053.txt bpetxt053.bin 1 
+lsb decode bpetxt053.bin text053.script.txt 1
+fc /b TEXT053.txt text053.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT054.DAT TEXT054.txt 3
+lsb encode TEXT054.txt bpetxt054.bin 1 
+lsb decode bpetxt054.bin text054.script.txt 1
+fc /b TEXT054.txt text054.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT055.DAT TEXT055.txt 3
+lsb encode TEXT055.txt bpetxt055.bin 1 
+lsb decode bpetxt055.bin text055.script.txt 1
+fc /b TEXT055.txt text055.script.txt
+pause
+
+REM Expected to fail.  Uses 1 non-ascii character
+lsb decode ios_utf8_script_us\TEXT056.DAT TEXT056.txt 3
+lsb encode TEXT056.txt bpetxt056.bin 1 
+lsb decode bpetxt056.bin text056.script.txt 1
+fc /b TEXT056.txt text056.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT057.DAT TEXT057.txt 3
+lsb encode TEXT057.txt bpetxt057.bin 1 
+lsb decode bpetxt057.bin text057.script.txt 1
+fc /b TEXT057.txt text057.script.txt
+pause
+
+REM Manual Intervention Required
+REM 003A is the second short in a 005C command if read linearly
+REM but at the same time its pointed to directly by one of the top level pointers
+REM By design, the automated parser will add 005C as a linear command
+REM then it will see that the top level pointer (located at 0x62C) does not have a matching command
+REM and then add the command in at the absolute value of 0x3BAA
+REM Due to the new compression, this results in a bunch of zero padding followed by 003A.
+REM To fix this, delete all the extra zero padding and the final 003A value.  Then fix the top level
+REM pointer to look at the correct 003A value in the file.  Should be the 2nd occurance.
+lsb decode ios_utf8_script_us\TEXT058.DAT TEXT058.txt 3
+lsb encode TEXT058.txt bpetxt058.bin 1 
+lsb decode bpetxt058.bin text058.script.txt 1
+fc /b TEXT058.txt text058.script.txt
+pause
+
+lsb decode ios_utf8_script_us\TEXT059.DAT TEXT059.txt 3
+lsb encode TEXT059.txt bpetxt059.bin 1 
+lsb decode bpetxt059.bin text059.script.txt 1
+fc /b TEXT059.txt text059.script.txt
+pause
